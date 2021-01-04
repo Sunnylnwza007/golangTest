afbb2b32a7fa72932989ba39b595aa33aefabb2a
Trinnakrit <trinnakrit.p@clicknext.com>
2021-01-04 16:38:12 +0700

    v0.003

--- app.go
+++ app.go
@@ -5,37 +5,8 @@ import (
 	"fmt"
 	"net/http"
 	// "log"
-	
+	"github.com/Sunnylnwza007/golangTest/models/webhook"
 )
-
-type Test struct {
-	Events []Event
-	Destination string
-}
-type Event struct{
-	Type string
-	ReplyToken string
-	Source Source
-	Timestamp int64
-	Mode string
-	Message Message
-
-}
-
-type Source struct{
-	UserId string
-	Type string
-	Address string
-	Latitude float32
-	Longitude float32
-}
-
-
-type Message struct{
-	Type string
-	Id string
-	
-}
 
 func handReq(){
 	
@@ -55,23 +26,8 @@ func addHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func testJson(w http.ResponseWriter, r *http.Request) {
+	var p models.Test
 	
-	// addBook := test{
-	// 			 Events: "test",
-	// 			 Destination: "1234",
-	// 		   }
-	// json.NewEncoder(w).Encode(addBook) // (1)
-	// decoder := json.NewDecoder(r.Body)
-	// var t test
-	// err := decoder.Decode(&t)
-	// if err != nil {
-    //     panic(err)
-    // }
-	// log.Println(t)
-	var p Test
-
-    // Try to decode the request body into the struct. If there is an error,
-    // respond to the client with the error message and a 400 status code.
     err := json.NewDecoder(r.Body).Decode(&p)
     if err != nil {
         http.Error(w, err.Error(), http.StatusBadRequest)
@@ -79,7 +35,7 @@ func testJson(w http.ResponseWriter, r *http.Request) {
     }
 
     // Do something with the Person struct...
-    fmt.Fprintf(w, "Person: %+v", p.Events)
+    fmt.Fprintf(w, "Person: %+v", p.Events[0].Message)
 	
  }
 
--- models/webhook/webhook_struct.go
+++ models/webhook/webhook_struct.go
@@ -1,30 +1,31 @@
 package models
 
 type Test struct {
-	Events []Event
-	Destination string
+	Events []Event `json:"event"`
+	Destination string `json:"destination"`
 }
 type Event struct{
-	Type string
-	ReplyToken string
-	Source Source
-	Timestamp int64
-	Mode string
-	Message Message
+	Type string `json:"Type"`
+	ReplyToken string `json:"replyToken"`
+	Source Source `json:"source"`
+	Timestamp int64 `json:"timestamp"`
+	Mode string `json:"mode"`
+	Message Message `json:"message"`
 
 }
 
 type Source struct{
-	UserId string
-	Type string
-	Address string
-	Latitude float32
-	Longitude float32
+	UserId string `json:"userId"`
+	Type string `json:"type"`
+	Address string `json:"address"`
+	Latitude float32 `json:"latitude"`
+	Longitude float32 `json:"longitude"`
 }
 
-
 type Message struct{
-	Type string
-	Id string
-	
+	Type string `json:"type"`
+	Id string `json:"id"`
+	Address string `json:"address"`
+	Latitude string `json:"latitude"`
+	Longitude string `json:"longitude"`
 }
